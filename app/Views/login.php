90f76f15076cd2eec12a4550624f4537be485a86
root <harrypurmanta@gmail.com>
2020-10-21 18:59:35 +0700

    view login

--- app/Views/login.php
+++ app/Views/login.php
@@ -4,6 +4,20 @@
 
 <!-- Mirrored from www.wrappixel.com/demos/admin-templates/admin-pro/main/pages-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 24 Jun 2020 15:08:29 GMT -->
 <head>
+    <style type="text/css">
+        ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+          color: white !important;
+          opacity: 1; /* Firefox */
+        }
+
+        :-ms-input-placeholder { /* Internet Explorer 10-11 */
+          color: white !important;
+        }
+
+        ::-ms-input-placeholder { /* Microsoft Edge */
+          color: white !important;
+        }
+    </style>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Tell the browser to be responsive to screen width -->
@@ -13,7 +27,7 @@
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="<?=base_url() ?>/assets/images/favicon.png">
     <title>Login Page</title>
-	<link rel="canonical" href="https://www.wrappixel.com/templates/adminpro/" />
+    <link rel="canonical" href="https://www.wrappixel.com/templates/adminpro/" />
     <!-- Bootstrap Core CSS -->
     <link href="<?=base_url() ?>/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- page css -->
@@ -45,33 +59,36 @@
     <!-- Main wrapper - style you can find in pages.scss -->
     <!-- ============================================================== -->
     <section id="wrapper">
-        <div class="login-register" style="background-image:url(<?=base_url() ?>/assets/images/background/login-register.jpg);">
-            <div class="login-box card">
-                <div class="card-body">
-                    <?= csrf_field() ?>
-                    <form class="form-horizontal form-material" id="loginform" action="/login/checklogin" method="post">
-                        <h3 class="box-title m-b-20">Sign In</h3>
-                        <div class="form-group ">
-                            <div class="col-xs-12">
-                                <input class="form-control" type="text" required="" name="username" id="username" placeholder="Username"> </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-xs-12">
-                                <input class="form-control" type="password" required="" name="password" id="password" placeholder="Password"> </div>
-                        </div>
-                        <div class="form-group text-center">
-                            <div class="col-xs-12 p-b-20">
-                                <button class="btn btn-block btn-lg btn-info btn-rounded" type="submit">Log In</button>
+        <div class="login-register" style="background-image:url(<?=base_url() ?>/assets/images/background/login-register.jpeg);">
+                <div class="login-box card" style="background:rgba(0,0,0,0.5); border-radius: 10px;">
+                    <div class="form-group m-t-20" style="margin-bottom: 0px;">
+                                <div class="col-sm-12 text-center" style="display: flex; justify-content: center; align-items: center;" align="center">
+                                    <img src="../assets/images/icon-baru-rev1.png" style="max-height: 100%; width: 80px; margin-right: 10px;">
+                                    <h3 style="font-weight: bold; font-size: 18px; color: white;">Mang PeDeKa Medis</h3>
+                                    <img src="../assets/images/Lambang_Polda_Sumsel.png" style="max-height: 100%; width: 60px; margin-left: 10px;">
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group m-b-0">
-                            <div class="col-sm-12 text-center">
-                                Anda lupa password? <a href="<?=base_url()?>login/forgotpassword" class="text-info m-l-5"><b>Lupa password</b></a>
+                    <div class="card-body">
+                        <?= csrf_field() ?>
+                        <form class="form-horizontal form-material" id="loginform" action="/login/checklogin" method="post">
+                            <div class="form-group ">
+                                <div class="col-xs-12">
+                                    <input style="color: white !important;" class="form-control" type="text" required="" name="username" id="username" placeholder="Username"> </div>
                             </div>
-                        </div>
-                    </form>
+                            <div class="form-group">
+                                <div class="col-xs-12">
+                                    <input class="form-control" type="password" required="" name="password" id="password" placeholder="Password"> </div>
+                            </div>
+                            <div class="form-group text-center">
+                                <div class="col-xs-12 p-b-20">
+                                    <button style="opacity: 1 !important;" class="btn btn-block btn-lg btn-info btn-rounded" type="submit">Log In</button>
+                                </div>
+                            </div>
+                            
+                        </form>
+                    </div>
                 </div>
-            </div>
+            
         </div>
     </section>
     
